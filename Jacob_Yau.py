f9e04c42cb9513daea4bc357a3c1f98ffb557eb2
Yaufu1234 <yautk@bedfordschool.org.uk>
2025-01-31 13:39:18 +0000

    Update Jacob_Yau.py

--- Jacob_Yau.py
+++ Jacob_Yau.py
@@ -3,11 +3,11 @@ from vex import *
 brain = Brain()
 
 controller_1 = Controller(PRIMARY) ##This is the controller object so we can get input from the controls
-conveyor = Motor(Ports.PORT1, GearSetting.RATIO_6_1, True)  #conveyor belt connect to port 1
-right_motor1 = Motor(Ports.PORT9, GearSetting.RATIO_18_1, True) 
-left_motor1 = Motor(Ports.PORT10, GearSetting.RATIO_18_1, False) #port 10 connect to the left, port 9 connect to the right
-right_motor2 = Motor(Ports.PORT12, GearSetting.RATIO_6_1, True) 
-left_motor2 = Motor(Ports.PORT13, GearSetting.RATIO_6_1, False)   
+conveyor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)  #conveyor belt connect to port 1
+right_motor1 = Motor(Ports.PORT9, GearSetting.RATIO_6_1, True) 
+left_motor1 = Motor(Ports.PORT10, GearSetting.RATIO_6_1, False) #port 10 connect to the left, port 9 connect to the right
+right_motor2 = Motor(Ports.PORT12, GearSetting.RATIO_18_1, True) 
+left_motor2 = Motor(Ports.PORT13, GearSetting.RATIO_18_1, False)   
 #swipe on 11, front black thingy on 13
 claw = Pneumatics(brain.three_wire_port.a)
 FrontSpindle = Motor(Ports.PORT2, GearSetting.RATIO_6_1, False)
@@ -26,9 +26,7 @@ def MoveSpindle():
         FrontSpindle.spin(FORWARD)
     else:
         FrontSpindle.stop()
-def test_move():
-    right_motor1.spin_for(FORWARD,1,TURNS,wait=False)
-    right_motor2.spin_for(FORWARD,6,TURNS,wait=False)
+
 def foward(length):
     length = length / 13.5
 
@@ -55,8 +53,8 @@ def left(degree):
     right_motor2.spin_for(FORWARD,degree*6,TURNS,wait=False)
     left_motor2.spin_for(REVERSE,degree*6,TURNS,wait=False)
 def autonomous():
-    right_motor1.set_velocity(65,PERCENT)
-    left_motor1.set_velocity(65,PERCENT)
+    right_motor1.set_velocity(90/3,PERCENT)
+    left_motor1.set_velocity(90/3,PERCENT)
     right_motor2.set_velocity(90,PERCENT)
     left_motor2.set_velocity(90,PERCENT)
     FrontSpindle.set_velocity(40,PERCENT)
@@ -70,17 +68,16 @@ def autonomous():
     
 
 def user_control():
-    FrontSpindle.set_velocity(50,PERCENT)
+    FrontSpindle.set_velocity(100,PERCENT)
     
     controller_1.buttonR1.pressed(MoveSpindle)
-
     controller_1.buttonL1.pressed(MoveClaw)
     
     while True:
 
     #MOTOR CONTROL
-        right_motor1.set_velocity((controller_1.axis3.position() - controller_1.axis4.position())/6, PERCENT)
-        left_motor1.set_velocity((controller_1.axis3.position() + controller_1.axis4.position())/6, PERCENT)
+        right_motor1.set_velocity((controller_1.axis3.position() - controller_1.axis4.position())/3, PERCENT)
+        left_motor1.set_velocity((controller_1.axis3.position() + controller_1.axis4.position())/3, PERCENT)
         right_motor2.set_velocity((controller_1.axis3.position() - controller_1.axis4.position()), PERCENT)
         left_motor2.set_velocity((controller_1.axis3.position() + controller_1.axis4.position()), PERCENT)
         conveyor.set_velocity((controller_1.axis2.position())*0.65,PERCENT)
