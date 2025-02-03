d033c8a27bd0f4dc4b432c0e1610aae00a96ced8
Yaufu1234 <yautk@bedfordschool.org.uk>
2025-02-03 10:12:37 +0000

    Update Jacob_Yau.py

--- Jacob_Yau.py
+++ Jacob_Yau.py
@@ -3,12 +3,11 @@ from vex import *
 brain = Brain()
 
 controller_1 = Controller(PRIMARY) ##This is the controller object so we can get input from the controls
-conveyor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)  #conveyor belt connect to port 1
+conveyor = Motor(Ports.PORT13, GearSetting.RATIO_6_1, True)  #conveyor belt connect to port 1
 right_motor1 = Motor(Ports.PORT9, GearSetting.RATIO_6_1, True) 
 left_motor1 = Motor(Ports.PORT10, GearSetting.RATIO_6_1, False) #port 10 connect to the left, port 9 connect to the right
 right_motor2 = Motor(Ports.PORT12, GearSetting.RATIO_18_1, True) 
-left_motor2 = Motor(Ports.PORT13, GearSetting.RATIO_18_1, False)   
-#swipe on 11, front black thingy on 13
+left_motor2 = Motor(Ports.PORT20, GearSetting.RATIO_18_1, False) 
 claw = Pneumatics(brain.three_wire_port.a)
 FrontSpindle = Motor(Ports.PORT2, GearSetting.RATIO_6_1, False)
 claw.close()
@@ -33,25 +32,25 @@ def foward(length):
     right_motor1.spin_for(FORWARD,length,TURNS,wait=False)  #one full turn will move 13.5 inches
     left_motor1.spin_for(FORWARD,length,TURNS,wait=False)
     right_motor2.spin_for(FORWARD,length*6,TURNS,wait=False)  #one full turn will move 13.5 inches
-    left_motor2.spin_for(FORWARD,length*6,TURNS,wait=False)
+    left_motor2.spin_for(FORWARD,length*6,TURNS,wait=True)
 def backward(length):
     length = length / 13.5
     right_motor1.spin_for(REVERSE,length,TURNS,wait=False)
     left_motor1.spin_for(REVERSE,length,TURNS,wait=False)
     right_motor2.spin_for(REVERSE,length*6,TURNS,wait=False)
-    left_motor2.spin_for(REVERSE,length*6,TURNS,wait=False)
+    left_motor2.spin_for(REVERSE,length*6,TURNS,wait=True)
 def right(degree):
     degree = degree / 90 * 0.96
     left_motor1.spin_for(FORWARD,degree,TURNS,wait=False) 
     right_motor1.spin_for(REVERSE,degree,TURNS,wait=False) #0.96 spin will turn 90 degrees
     left_motor2.spin_for(FORWARD,degree*6,TURNS,wait=False) 
-    right_motor2.spin_for(REVERSE,degree*6,TURNS,wait=False)
+    right_motor2.spin_for(REVERSE,degree*6,TURNS,wait=True)
 def left(degree):
     degree = degree / 90 * 0.96
     right_motor1.spin_for(FORWARD,degree,TURNS,wait=False)
     left_motor1.spin_for(REVERSE,degree,TURNS,wait=False)
     right_motor2.spin_for(FORWARD,degree*6,TURNS,wait=False)
-    left_motor2.spin_for(REVERSE,degree*6,TURNS,wait=False)
+    left_motor2.spin_for(REVERSE,degree*6,TURNS,wait=True)
 def autonomous():
     right_motor1.set_velocity(90/3,PERCENT)
     left_motor1.set_velocity(90/3,PERCENT)
@@ -95,8 +94,3 @@ comp = Competition(user_control, autonomous)
 
 # actions to do when the program starts
 brain.screen.clear_screen()
-
-
-
-
-
