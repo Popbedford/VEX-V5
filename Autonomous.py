33ac831a981411233f6d6aa168fc3c0eb4db734f
Yaufu1234 <yautk@bedfordschool.org.uk>
2025-02-03 10:10:03 +0000

    Update Autonomous.py

--- Autonomous.py
+++ Autonomous.py
@@ -1,66 +1,95 @@
 from vex import *
 
+brain = Brain()
+
 controller_1 = Controller(PRIMARY) ##This is the controller object so we can get input from the controls
-conveyer = Motor(Ports.PORT1, GearSetting.RATIO_6_1, True)  #conveyor belt connect to port 1
-right_motor = Motor(Ports.PORT9, GearSetting.RATIO_18_1, True) 
-left_motor = Motor(Ports.PORT10, GearSetting.RATIO_18_1, False)   #port 10 connect to the left, port 9 connect to the right
+conveyor = Motor(Ports.PORT13, GearSetting.RATIO_6_1, True)  #conveyor belt connect to port 1
+right_motor1 = Motor(Ports.PORT9, GearSetting.RATIO_6_1, True) 
+left_motor1 = Motor(Ports.PORT10, GearSetting.RATIO_6_1, False) #port 10 connect to the left, port 9 connect to the right
+right_motor2 = Motor(Ports.PORT12, GearSetting.RATIO_18_1, True) 
+left_motor2 = Motor(Ports.PORT20, GearSetting.RATIO_18_1, False) 
 #swipe on 11, front black thingy on 13
-
-FrontSpindle = Motor(Ports.PORT13, GearSetting.RATIO_18_1, False)
-
-claw.set(False)
-
-def conveyerstart():
-    conveyer.spin(FORWARD)
-def conveyerreverse():
-    conveyer.spin(REVERSE)
-def conveyerstop():
-    conveyer.stop()
-def spindlestart():
-    FrontSpindle.spin(FORWARD)
-def spindlestop():
-    FrontSpindle.stop()
-
-
+claw = Pneumatics(brain.three_wire_port.a)
+FrontSpindle = Motor(Ports.PORT2, GearSetting.RATIO_6_1, False)
+claw.close()
 def ClawUp():
-    claw.set(False)
+    claw.close()
 def ClawDown():
-    claw.set(True)
-    
-
-def SwipeUpDown():
-    if swipe.position(DEGREES) == 0:
-        swipe.spin_to_position(90,DEGREES,wait=True)
+    claw.open()
+def MoveClaw():
+    if claw.value() == 0:
+        claw.open()
     else:
-        swipe.spin_to_position(0,DEGREES,wait=True)
-
-
+        claw.close()
+def MoveSpindle():
+    if FrontSpindle.velocity() == 0:
+        FrontSpindle.spin(FORWARD)
+    else:
+        FrontSpindle.stop()
 def foward(length):
     length = length / 13.5
-    right_motor.spin_for(FORWARD,length,TURNS,wait=False)  #one full turn will move 13.5 inches
-    left_motor.spin_for(FORWARD,length,TURNS,wait=False)
+
+    right_motor1.spin_for(FORWARD,length,TURNS,wait=False)  #one full turn will move 13.5 inches
+    left_motor1.spin_for(FORWARD,length,TURNS,wait=False)
+    right_motor2.spin_for(FORWARD,length,TURNS,wait=False)  #one full turn will move 13.5 inches
+    left_motor2.spin_for(FORWARD,length,TURNS,wait=True)
 def backward(length):
     length = length / 13.5
-    right_motor.spin_for(REVERSE,length,TURNS,wait=False)
-    left_motor.spin_for(REVERSE,length,TURNS,wait=False)
+    right_motor1.spin_for(REVERSE,length,TURNS,wait=False)
+    left_motor1.spin_for(REVERSE,length,TURNS,wait=False)
+    right_motor2.spin_for(REVERSE,length,TURNS,wait=False)
+    left_motor2.spin_for(REVERSE,length,TURNS,wait=True)
 def right(degree):
     degree = degree / 90 * 0.96
-    left_motor.spin_for(FORWARD,degree,TURNS,wait=False) 
-    right_motor.spin_for(REVERSE,degree,TURNS,wait=False) #0.96 spin will turn 90 degrees
+    left_motor1.spin_for(FORWARD,degree,TURNS,wait=False) 
+    right_motor1.spin_for(REVERSE,degree,TURNS,wait=False) #0.96 spin will turn 90 degrees
+    left_motor2.spin_for(FORWARD,degree,TURNS,wait=False) 
+    right_motor2.spin_for(REVERSE,degree,TURNS,wait=True)
 def left(degree):
     degree = degree / 90 * 0.96
-    right_motor.spin_for(FORWARD,degree,TURNS,wait=False)
-    left_motor.spin_for(REVERSE,degree,TURNS,wait=False)
+    right_motor1.spin_for(FORWARD,degree,TURNS,wait=False)
+    left_motor1.spin_for(REVERSE,degree,TURNS,wait=False)
+    right_motor2.spin_for(FORWARD,degree,TURNS,wait=False)
+    left_motor2.spin_for(REVERSE,degree,TURNS,wait=True)
 
 
-FrontSpindle.set_velocity(25,PERCENT)
-conveyer.set_velocity(50,PERCENT) #works at 50%
-conveyer.spin(FORWARD)
-FrontSpindle.spin(FORWARD)
-foward(58.42)
-right(135)
-backward(17)
+right_motor1.set_velocity(90/3,PERCENT)
+left_motor1.set_velocity(90/3,PERCENT)
+right_motor2.set_velocity(90,PERCENT)
+left_motor2.set_velocity(90,PERCENT)
+FrontSpindle.set_velocity(40,PERCENT)
+conveyor.set_velocity(50,PERCENT) #works at 50%
+backward(33)
+wait(3,SECONDS)
 ClawDown()
+conveyor.spin(FORWARD)
+FrontSpindle.spin(FORWARD)
+right(90)
+foward(23)
+
+    
+
+# def user_control():
+#     FrontSpindle.set_velocity(50,PERCENT)
+#     controller_1.buttonR1.pressed(MoveSpindle)
+    
+#     controller_1.buttonL1.pressed(MoveClaw)
+    
+#     while True:
+
+#     ##MOTOR CONTROL
+#         right_motor1.set_velocity((controller_1.axis3.position() - controller_1.axis4.position())*0.5, PERCENT)
+#         left_motor1.set_velocity((controller_1.axis3.position() + controller_1.axis4.position())*0.5, PERCENT)
+#         right_motor2.set_velocity((controller_1.axis3.position() - controller_1.axis4.position()), PERCENT)
+#         left_motor2.set_velocity((controller_1.axis3.position() + controller_1.axis4.position()), PERCENT)
+#         conveyor.set_velocity((controller_1.axis2.position())*0.65,PERCENT)
+#         right_motor1.spin(FORWARD)
+#         left_motor1.spin(FORWARD)
+#         right_motor2.spin(FORWARD)
+#         left_motor2.spin(FORWARD)
+#         conveyor.spin(FORWARD)
+
+#         wait(5, MSEC)
 
 
 
@@ -68,10 +97,5 @@ ClawDown()
 
 
 
-# Main Controller loop to set motors to controller axis postiions
-while True:
 
-    ##MOTOR CONTROL
-    FrontSpindle.spin(FORWARD)
-    conveyer.spin(FORWARD)
-    wait(5, MSEC)
+
