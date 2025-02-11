3b1ce48b0aa40c52f024d704f5d7af7f9d687fb3
Yaufu1234 <yautk@bedfordschool.org.uk>
2025-02-11 14:22:21 +0000

    Update Autonomous.py

--- Autonomous.py
+++ Autonomous.py
@@ -3,7 +3,7 @@ from vex import *
 brain = Brain()
 
 controller_1 = Controller(PRIMARY) ##This is the controller object so we can get input from the controls
-conveyor = Motor(Ports.PORT13, GearSetting.RATIO_6_1, True)  #conveyor belt connect to port 1
+conveyor = Motor(Ports.PORT13, GearSetting.RATIO_18_1, True)  #conveyor belt connect to port 1
 right_motor1 = Motor(Ports.PORT9, GearSetting.RATIO_6_1, True) 
 left_motor1 = Motor(Ports.PORT10, GearSetting.RATIO_6_1, False) #port 10 connect to the left, port 9 connect to the right
 right_motor2 = Motor(Ports.PORT12, GearSetting.RATIO_18_1, True) 
@@ -59,45 +59,43 @@ def left(degree):
     right_motor2.spin_for(FORWARD,degree,TURNS,wait=False)
     left_motor2.spin_for(REVERSE,degree,TURNS,wait=True)
 
-right_motor1.set_velocity(45/3*1.05,PERCENT)
-left_motor1.set_velocity(45/3,PERCENT)
-right_motor2.set_velocity(45*1.05,PERCENT)
-left_motor2.set_velocity(45,PERCENT)
-FrontSpindle.set_velocity(40,PERCENT)
-conveyor.set_velocity(50,PERCENT) #works at 50%
-backward(33)
-ClawDown()
-SpinConveyor()
-SpinSpindle()
-right(105)
-foward(23)
-
-
-# def user_control():
-#     FrontSpindle.set_velocity(100,PERCENT)
-    
-#     controller_1.buttonR1.pressed(MoveSpindle)
-#     controller_1.buttonL1.pressed(MoveClaw)
-    
-#     while True:
-
-#     #MOTOR CONTROL
-#         right_motor1.set_velocity((controller_1.axis3.position() - controller_1.axis4.position())/3*1.05, PERCENT)
-#         left_motor1.set_velocity((controller_1.axis3.position() + controller_1.axis4.position())/3, PERCENT)
-#         right_motor2.set_velocity((controller_1.axis3.position() - controller_1.axis4.position())*1.05, PERCENT)
-#         left_motor2.set_velocity((controller_1.axis3.position() + controller_1.axis4.position()), PERCENT)
-#         conveyor.set_velocity((controller_1.axis2.position())*0.65,PERCENT)
-#         right_motor1.spin(FORWARD)
-#         left_motor1.spin(FORWARD)
-#         right_motor2.spin(FORWARD)
-#         left_motor2.spin(FORWARD)
-#         conveyor.spin(FORWARD)
-
-#         wait(5, MSEC)
-
-
-# # create competition instance
-# comp = Competition(user_control, autonomous)
-
-# # actions to do when the program starts
-# brain.screen.clear_screen()
+def autonomous():
+    ClawUp()
+    right_motor1.set_velocity(60/3*1.05,PERCENT)
+    left_motor1.set_velocity(60/3,PERCENT)
+    right_motor2.set_velocity(60,PERCENT)
+    left_motor2.set_velocity(60,PERCENT)
+    FrontSpindle.set_velocity(40,PERCENT)
+    conveyor.set_velocity(75,PERCENT) #works at 50%
+    SpinConveyor()
+    SpinSpindle()
+    backward(10)
+    right(90)
+    backward(23.08)
+    right(45)
+    backward(33.64)
+    ClawUp()
+    foward(32.64)
+    right(135)
+    backward(70)
+    ClawDown()
+    backward(23.08)
+    left(45)
+    backward(33.64)
+    ClawUp()
+    foward(131)
+    right(90)
+    backward(33)
+    ClawDown()
+    left(45)
+    backward(23.08*2)
+    right(45)
+    backward(33)
+    ClawUp()
+    right(135)
+    backward(35)
+    ClawDown()
+    backward(93)
+    left(45)
+    backward(15)
+    ClawUp()
