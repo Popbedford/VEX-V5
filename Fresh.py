32f69c59748caee7b6e5afdd79d88f24b4022e5f
Popdagod <ImmarapornP@bedfordschool.org.uk>
2025-10-04 11:14:58 +0100

    Update Fresh.py

--- Fresh.py
+++ Fresh.py
@@ -5,16 +5,16 @@ brain = Brain()
 controller_1 = Controller(PRIMARY) ##This is the controller object so we can get input from the controls
 conveyor = Motor(Ports.PORT1, GearSetting.RATIO_6_1, True)  #conveyor belt connect to port 1
 spindle = Motor(Ports.PORT2, GearSetting.RATIO_6_1, True)
-right_motor_front = Motor(Ports.PORT9, GearSetting.RATIO_6_1, False) 
-left_motor_front = Motor(Ports.PORT10, GearSetting.RATIO_6_1, True) #port 10 connect to the left, port 9 connect to the right
-right_motor_rear = Motor(Ports.PORT12, GearSetting.RATIO_6_1, False) 
-left_motor_rear = Motor(Ports.PORT13, GearSetting.RATIO_6_1, True) 
+right_motor_front = Motor(Ports.PORT9, GearSetting.RATIO_6_1, True) 
+left_motor_front = Motor(Ports.PORT10, GearSetting.RATIO_6_1, False) #port 10 connect to the left, port 9 connect to the right
+right_motor_rear = Motor(Ports.PORT12, GearSetting.RATIO_6_1, True) 
+left_motor_rear = Motor(Ports.PORT13, GearSetting.RATIO_6_1, False) 
+Newmotor = Motor(Ports.PORT19, GearSetting.RATIO_6_1, True) 
 push_motor = Pneumatics(brain.three_wire_port.a)
 colour_sensor = Optical(Ports.PORT20)
 
-
 def foward(length):
-    length = length / 13.5
+    
     right_motor_front.spin_for(FORWARD,length,TURNS,wait=False)  #one full turn will move 13.5 inches
     left_motor_front.spin_for(FORWARD,length,TURNS,wait=False)
     right_motor_rear.spin_for(FORWARD,length,TURNS,wait=False)  #one full turn will move 13.5 inches
@@ -26,13 +26,13 @@ def backward(length):
     right_motor_rear.spin_for(REVERSE,length,TURNS,wait=False)
     left_motor_rear.spin_for(REVERSE,length,TURNS,wait=True)
 def right(degree):
-    degree = degree / 90 * 0.96
+    
     left_motor_front.spin_for(FORWARD,degree,TURNS,wait=False) 
     right_motor_front.spin_for(REVERSE,degree,TURNS,wait=False) #0.96 spin will turn 90 degrees
     left_motor_rear.spin_for(FORWARD,degree,TURNS,wait=False) 
     right_motor_rear.spin_for(REVERSE,degree,TURNS,wait=True)
 def left(degree):
-    degree = degree / 90 * 0.96
+    
     right_motor_front.spin_for(FORWARD,degree,TURNS,wait=False)
     left_motor_front.spin_for(REVERSE,degree,TURNS,wait=False)
     right_motor_rear.spin_for(FORWARD,degree,TURNS,wait=False)
@@ -40,46 +40,59 @@ def left(degree):
 
 def CheckColour():
     brain.screen.clear_screen()
+    Newmotor.set_velocity(100, RPM)
     if colour_sensor.is_near_object():
         if colour_sensor.color() == Color.BLUE:
+            Newmotor.spin(FORWARD)
             brain.screen.print("blue")
         else:
+            Newmotor.spin(REVERSE)
             brain.screen.print("red")
-        
-        # Only detect colors if saturation is high enough
 
+def AUTONOMOUSCHECKCOLOUR():
+    brain.screen.clear_screen()
+    Newmotor.set_velocity(100, RPM)
+    if colour_sensor.is_near_object():
+        if colour_sensor.color() == Color.BLUE:
+            right_motor_front.spin(FORWARD)
+            brain.screen.print("blue")
+        else:
+            left_motor_front.spin(FORWARD)
+            brain.screen.print("red")
+        # Only detect colors if saturation is high enough
 def MoveConveyor():
     if conveyor.velocity() == 0:
         conveyor.spin(FORWARD)
     else:
         conveyor.stop()
                 
-def MoveSpindle():
-    if spindle.velocity() == 0:
-        spindle.spin(FORWARD)
-    else:
-        spindle.stop()
+
        
         
 
-def autonomous():
-    pass
+def autonomous(): #1 foward = 11 inches, 1 turn is around 53 degrees
+    right_motor_front.set_velocity(67, PERCENT)
+    left_motor_front.set_velocity(67, PERCENT)
+    right_motor_rear.set_velocity(67, PERCENT)
+    left_motor_rear.set_velocity(67, PERCENT)
+    foward(45/11)
+    left(1.3)
+    AUTONOMOUSCHECKCOLOUR()
     
 
 
 def user_control():
     brain.screen.clear_screen()
     controller_1.buttonA.pressed(CheckColour)
-    controller_1.buttonB.pressed(MoveConveyor)
-    controller_1.buttonY.pressed(MoveSpindle)
+    
 
     while True:
 
     #MOTOR CONTROL
-        right_motor_front.set_velocity((controller_1.axis3.position() + controller_1.axis4.position()), PERCENT)
-        left_motor_front.set_velocity((controller_1.axis3.position() - controller_1.axis4.position()), PERCENT)
-        right_motor_rear.set_velocity((controller_1.axis3.position() + controller_1.axis4.position()), PERCENT)
-        left_motor_rear.set_velocity((controller_1.axis3.position() - controller_1.axis4.position()), PERCENT)
+        right_motor_front.set_velocity((controller_1.axis3.position() - controller_1.axis4.position()), PERCENT)
+        left_motor_front.set_velocity((controller_1.axis3.position() + controller_1.axis4.position()), PERCENT)
+        right_motor_rear.set_velocity((controller_1.axis3.position() - controller_1.axis4.position()), PERCENT)
+        left_motor_rear.set_velocity((controller_1.axis3.position() + controller_1.axis4.position()), PERCENT)
         right_motor_front.spin(FORWARD)
         left_motor_front.spin(FORWARD)
         right_motor_rear.spin(FORWARD)
