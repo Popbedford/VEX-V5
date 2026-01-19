4f794ed773027a02c0721965695984149425c749
Nikhil-KOAS <NikhilDasgupta44@hotmail.com>
2026-01-19 16:44:33 +0000

    Update main.py

--- VEX-Y9/src/main.py
+++ VEX-Y9/src/main.py
@@ -11,14 +11,15 @@
 from vex import *
 controller=Controller()
 brain = Brain()
-left_motor_front = Motor(Ports.PORT18,GearSetting.RATIO_18_1,False)
-left_motor_rear = Motor(Ports.PORT17,GearSetting.RATIO_18_1,True)
-right_motor_front = Motor(Ports.PORT20,GearSetting.RATIO_18_1,True)
-right_motor_rear = Motor(Ports.PORT19,GearSetting.RATIO_18_1,False)
+left_motor_front = Motor(Ports.PORT3,GearSetting.RATIO_18_1,False)
+left_motor_rear = Motor(Ports.PORT8,GearSetting.RATIO_18_1,True)
+right_motor_front = Motor(Ports.PORT7,GearSetting.RATIO_18_1,True)
+right_motor_rear = Motor(Ports.PORT1,GearSetting.RATIO_18_1,False)
 left_motors = MotorGroup(left_motor_front, left_motor_rear)
 right_motors = MotorGroup(right_motor_front, right_motor_rear)
 
-conveyor=Motor(Ports.PORT15,GearSetting.RATIO_6_1)
+conveyor=Motor(Ports.PORT9,GearSetting.RATIO_6_1)
+rubber_wheel_motor=Motor(Ports.PORT2, GearSetting.RATIO_18_1)
 
 drivetrain = DriveTrain(left_motors, right_motors, 300, 320, 320, MM, 1)
 
@@ -52,7 +53,13 @@ def user_control():
             conveyor.set_velocity(50, PERCENT)
         else:
             conveyor.set_velocity(0, PERCENT)
-        
+        #rubber
+        if controller.buttonL2.pressing()==True:
+            rubber_wheel_motor.set_velocity(-50, PERCENT)
+        elif controller.buttonR2.pressing()==True:
+            rubber_wheel_motor.set_velocity(50, PERCENT)
+        else:
+            rubber_wheel_motor.set_velocity(0, PERCENT)
 
 # create competition instance aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 comp = Competition(user_control, autonomous)
@@ -64,6 +71,8 @@ brain.screen.print("hello")
 drivetrain.drive(FORWARD)
 drivetrain.turn(RIGHT)
 conveyor.spin(FORWARD)
+rubber_wheel_motor.spin(REVERSE)
 
-brain.screen.print("I Am The Cadasio Conveyer Bot 2000 With The Low Taper Fade Amd the 67 Trim Fro Tescos")
-brain.screen.print("My Creators Were Gene Baker, Nikhil Dasgupta And Lesser So Ilyas Hosein And Least of All Ethan Lad")
+controller.screen.print("I Am The Cadasio Conveyer Bot 2000 With The Low Taper Fade Amd the 67 Trim Fro Tescos")
+controller.screen.new_line()
+controller.screen.print("My Creators Were Gene Baker, Nikhil Dasgupta And Lesser So Ilyas Hosein")
