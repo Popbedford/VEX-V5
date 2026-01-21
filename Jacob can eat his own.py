8d79b2afec91e78de68ead5aa72c3467f45fe0aa
Popdagod <ImmarapornP@bedfordschool.org.uk>
2026-01-21 13:15:31 +0000

    Update Jacob can eat his own.py

--- Jacob can eat his own.py
+++ Jacob can eat his own.py
@@ -36,6 +36,29 @@ motor_group_right = MotorGroup(
 
 Drivetrain1 = DriveTrain(motor_group_left, motor_group_right)
 
+def AutonForward(length):
+    BotDriveTrain.set_drive_velocity(100,PERCENT)
+    BotDriveTrain.drive_for(FORWARD,length,INCHES)
+
+def AutonBackward(length):
+    BotDriveTrain.set_drive_velocity(100,PERCENT)
+    BotDriveTrain.drive_for(REVERSE,length,INCHES)
+
+def AutonRight(degree):
+    BotDriveTrain.set_drive_velocity(100,PERCENT)
+    BotDriveTrain.turn_for(RIGHT,90,DEGREES)
+
+def AutonLeft(degree):
+    BotDriveTrain.set_drive_velocity(100,PERCENT)
+    BotDriveTrain.turn_for(LEFT,90,DEGREES)
+
+def MoveSpindleForward():
+    if Spindle1.velocity() == 0:
+        Spindles.set_velocity(100,PERCENT)
+        Spindles.spin(FORWARD)
+    else:
+        Spindles.stop()
+
 # input should be in inches, then will convert into turns
 def foward(length):
     length = length/12.5
