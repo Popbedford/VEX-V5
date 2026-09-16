d67f3a32cd897dddae419ed3e29371eac2506399
Popdagod <ImmarapornP@bedfordschool.org.uk>
2026-09-16 13:29:32 +0100

    Refactor motor control in user_control function
    Updated motor control logic to use axis positions from the controller.

--- Test1.py
+++ Test1.py
@@ -1,3 +1,13 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Popdham                                                      #
+# 	Created:      9/16/2026, 1:21:08 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
 from vex import *
 
 brain = Brain()
@@ -6,8 +16,7 @@ controller_1 = Controller(PRIMARY) ##This is the controller object so we can get
 elevenwattmotor = Motor(Ports.PORT19, GearSetting.RATIO_18_1, True) 
 fivewattmotor = Motor(Ports.PORT19, GearSetting.RATIO_18_1, True)
 
-def foward(length):
-    
+
 def autonomous(): #1 foward = 11 inches, 1 turn is around 53 degrees
     pass
     
@@ -16,8 +25,8 @@ def autonomous(): #1 foward = 11 inches, 1 turn is around 53 degrees
 def user_control():
     brain.screen.clear_screen()
     while True:
-        elevenwattmotor.set_velocity(controller_1,axis1)
-        fivewattmotor.set_velocity(90, PERCENT)
+        elevenwattmotor.set_velocity(controller_1.axis3.position()-controller_1.axis4.position())
+        fivewattmotor.set_velocity(controller_1.axis1.position()-controller_1.axis2.position())
     #MOTOR CONTROL
         
         elevenwattmotor.spin(FORWARD)
